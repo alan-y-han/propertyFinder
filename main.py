51f248e63ee2f71cf6fa50ec27d751746141c853
Alan Han <alan.y.han@gmail.com>
2019-09-02 20:33:54 +0100

    More easily set search parameters

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-
-import requests
 import bs4
+import requests
+import time
 
 
 def getPlaces(soup, places):
@@ -10,7 +9,7 @@ def getPlaces(soup, places):
         places.add(url.attrs['href'])
 
 
-def isNextButton(soup):
+def getNextURL(soup):
     linkbar = soup.find('div', attrs={'class': 'paginate bg-muted'})
     links = linkbar.findAll('a')
     for link in links:
@@ -18,26 +17,17 @@ def isNextButton(soup):
             return True, "https://www.primelocation.com" + link.attrs['href']
     return False, None
 
-    # page = soup.find('div', attrs={'class': 'paginate bg-muted'})
-    # return "Next" in page.text
-
-
-def getNextURL(soup):
-    page = soup.find('div', attrs={'class': 'paginate bg-muted'})
-    return "https://www.primelocation.com" + list(page.children)[-2].attrs['href']
-
 
 def scrapeURL(URL):
     places = set()
 
-    # URL = "https://www.primelocation.com/to-rent/property/london/worship-street/ec2a-2ba/?beds_min=3&price_max=3500&transport_type=public_transport&identifier=london%2Fworship-street%2Fec2a-2ba&duration=1800page_size%3D50&q=EC2A%202BA&results_sort=newest_listings&search_source=refine&radius=0&price_frequency=per_month&view_type=list&page_size=50&pn=13"
     soup = bs4.BeautifulSoup(requests.get(URL).text, features="html.parser")
     getPlaces(soup, places)
 
     while True:
-        time.sleep(0.1)
+        time.sleep(0.1)  # guard against bugs
 
-        nextExists, URL = isNextButton(soup)
+        nextExists, URL = getNextURL(soup)
         if not nextExists:
             break
 
@@ -49,14 +39,28 @@ def scrapeURL(URL):
 
 if __name__ == '__main__':
 
-    AmazonURL = "https://www.primelocation.com/to-rent/property/london/worship-street/ec2a-2ba/?beds_min=3&price_max=3500&transport_type=public_transport&identifier=london%2Fworship-street%2Fec2a-2ba&duration=2700page_size=50&q=EC2A%202BA&results_sort=newest_listings&search_source=refine&radius=0&price_frequency=per_month&view_type=list&page_size=50"
-    AmazonPlaces = scrapeURL(AmazonURL)
-    print(AmazonPlaces)
+    commonURL = "https://www.primelocation.com/to-rent/property/"
 
-    ImperialURL = "https://www.primelocation.com/to-rent/property/sw7-2az/?beds_min=3&price_max=3500&transport_type=public_transport&identifier=london%2Fworship-street%2Fec2a-2ba&duration=2700page_size=50&q=EC2A%202BA&results_sort=newest_listings&search_source=refine&radius=0&price_frequency=per_month&view_type=list&page_size=50"
-    ImperialPlaces = scrapeURL(ImperialURL)
-    print(ImperialPlaces)
+    locations = [
+        "london/worship-street/ec2a-2ba/?",  # Principal Place (Amazon)
+        "sw7-2az/?"                          # Imperial College
+    ]
 
-    commonPlaces = AmazonPlaces & ImperialPlaces
+    params = [
+        "beds_min=3",
+        "duration=2200",  # max commute time (in seconds)
+        "price_frequency=per_month",
+        "price_max=3500",
+        "results_sort=newest_listings",
+        "search_source=travel-time",
+        "transport_type=public_transport",
+        "page_size=50"  # 100 seems to be the unofficial limit?
+    ]
 
-    print(commonPlaces)
+    placeSets = []
+
+    for location in locations:
+        URL = commonURL + location + "&".join(params)
+        placeSets.append(scrapeURL(URL))
+
+    print(set.intersection(*placeSets))
